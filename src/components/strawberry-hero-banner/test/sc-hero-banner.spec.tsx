56182aed1bd79071f1b3daca68b11b43ffc0d95d
Liam Silk <liam.silk@strawberry.co.uk>
2021-12-06 15:34:15 +0000

    fix test

--- src/components/strawberry-hero-banner/test/sc-hero-banner.spec.tsx
+++ src/components/strawberry-hero-banner/test/sc-hero-banner.spec.tsx
@@ -8,7 +8,7 @@ describe('strawberry-hero-banner', () => {
       html: `<sc-hero-banner></sc-hero-banner>`,
     });
     expect(page.root).toEqualHtml(`
-      <sc-hero-banner>
+      <sc-hero-banner style="--sc-hero-banner-align: flex-start; --sc-hero-banner-justify: flex-start; --sc-hero-banner-text-align: left;">
         <mock:shadow-root>
           <div class="container">
             <slot></slot>
